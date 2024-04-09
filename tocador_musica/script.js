490eaa5819f4ffc7e39ad93e9bd6ddae3e858671
José Renildo <renildojose7@gmail.com>
2024-04-09 14:55:20 -0300

    Update script.js
    configurado botão shuffle por completo. Função para clicar, ativar e botão ficar verde, e desativar, voltando ao normal.

--- tocador_musica/script.js
+++ tocador_musica/script.js
@@ -6,6 +6,9 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
 
 // Variáveis criadas para apresentação da música
 const dustToDust = {
@@ -26,8 +29,11 @@ const waitAndBleed = {
     file: 'Slipknot_capa',
 };
 
-let isPlaying = false
-const playlist = [dustToDust, dontStay, waitAndBleed];
+let isPlaying = false;
+let isShuffled = false;
+
+const orginalPlaylist = [dustToDust, dontStay, waitAndBleed];
+let sortedPlaylist = [...orginalPlaylist]
 let index = 0;
 
 // FUNÇÕES !!
@@ -61,15 +67,15 @@ function playPauseDecider(){
 
 // função responsável por apresentar cada card/música com suas respectivas características
 function initializeSong(){
-    cover.src = `images/covers/${playlist[index].file}.jpg`;
-    song.src = `songs/${playlist[index].songName}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
+    cover.src = `images/covers/${sortedPlaylist[index].file}.jpg`;
+    song.src = `songs/${sortedPlaylist[index].songName}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
 }
 
 function previousSong() {
     if(index === 0){
-        index = playlist.length -1;
+        index = sortedPlaylist.length -1;
     }
     else{
         index -= 1;
@@ -79,7 +85,7 @@ function previousSong() {
 }
 
 function nextSong() {
-    if(index === playlist.length -1) {
+    if(index === sortedPlaylist.length -1) {
         index = 0;
     }
     else{
@@ -89,9 +95,50 @@ function nextSong() {
     playSong();
 }
 
+// função para barra de progresso
+function updateProgressBar() {
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event) {
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)* song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+    let size = preShuffleArray.length;
+    let currentIndex = size - 1
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()* size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButtonClicked () {
+    if(isShuffled === false) {
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add('button-active');
+    }
+    else {
+        isShuffled = false;
+        sortedPlaylist = [...orginalPlaylist];
+        shuffleButton.classList.remove('button-active');
+    }
+}
+
 initializeSong();
 
 // ação dos botões
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong)
-next.addEventListener('click', nextSong)
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
